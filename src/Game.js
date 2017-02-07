bc9fc0765d11fb7082667bb51cc9d637f4326d86
Graham Wood <graham@builtprecise.com>
2017-02-07 13:27:34 -0600

    Removed some sample code. Added Game and OnOffToggle Component Skelaton

---
+++ src/Game.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import OnOffToggle from './OnOffToggle'
+
+
+class Game extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Game Component</h1>
+        <OnOffToggle />
+      </div>
+    )
+  }
+}
+
+export default Game;
---
+++ src/OnOffToggle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class OnOffToggle extends React.Component {
+  render () {
+    return (
+      <h2>OnOff Toggle</h2>
+    )
+  }
+}
+
+export default OnOffToggle;
